281f5c5a5e6558187d6412267c450ada0eb66b38
tobitprince <prince.tobit@strathmore.edu>
2024-12-22 14:41:18 +0300

    Initial commit

---
+++ backend/src/test/java/com/princetobit/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princetobit.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
