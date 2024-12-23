91a460d5208d2ad1685f587df2812d7ae2e30677
tobitprince <prince.tobit@strathmore.edu>
2024-12-23 22:26:14 +0300

    Implement movie fetching in frontend; configure axios with base URL and headers; add layout component for routing

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'',
-    headers:{"ngrok-skip-browser-warning": "true"}
+    baseURL: 'http://localhost:8080/api/v1',
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
