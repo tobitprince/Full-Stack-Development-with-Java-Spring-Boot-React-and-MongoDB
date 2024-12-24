5d872e6f8377f367b91bdf761f642f3cc534284a
tobitprince <prince.tobit@strathmore.edu>
2024-12-25 00:51:28 +0300

    Add NotFound and Reviews components; refactor Layout component; update Header branding; enhance App routing and styling

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
             <Navbar.Brand href="/" style={{"color": "gold"}}>
-                <FontAwesomeIcon icon = {faVideoSlash}/>Gold
+                <FontAwesomeIcon icon = {faVideoSlash}/>Tobitube
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll"/>
             <Navbar.Collapse id="navbarScroll">
